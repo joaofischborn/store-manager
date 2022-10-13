75a3839c7628ef9ecfaf6fa0974874a19d309d75
João Vitor Fischborn <joaovitorfischborn@icloud.com>
2022-10-13 17:49:16 -0300

    Requisito 01

---
+++ src/controllers/products.controller.js
@@ -0,0 +1,18 @@
+const productsService = require('../services');
+// const errorMap = require('../utils/errorMap');
+
+const listAllProducts = async (_req, res) => {
+  const { type, message } = await productsService.findAll();
+  if (type) return res.status(404).json(message);
+  res.status(200).json(message);
+};
+
+const findById = async (req, res) => {
+  const { id } = req.params;
+  const { type, message } = await productsService.findById(id);
+  console.log(message);
+  if (type) return res.status(404).json({ message });
+  res.status(200).json(message);
+};
+
+module.exports = { listAllProducts, findById };
